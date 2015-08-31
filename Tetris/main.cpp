49a76a812ac7861c25622edb93ee68cecb08be60
Sam-Bates <batez1993@gmail.com>
2015-08-31 16:49:21 +1200

    Tidy up

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -285,8 +285,8 @@ int initialPlace  [7 /*kind */ ][4 /* r2otation */ ][2 /* position */] =
 #include <time.h>
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 720;
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
 
 
 //Starts up SDL and creates window
@@ -298,18 +298,19 @@ bool loadMedia();
 //Frees media and shuts down SDL
 void close();
 
-const int BOARD_WIDTH = 12; //in blocks
-const int BOARD_HEIGHT = 22; //in blocks
-const int POS_FREE = 0;
+const int BOARD_WIDTH = 11; //in blocks
+const int BOARD_HEIGHT = 21; //in blocks
+const int POS_FREE = 0; 
 const int POS_TAKEN = 1;
 const int BORDER = 2;
 const int PIECE_BLOCKS = 5; // in blocks
+const int MAGNIFICATION = 11;
 
-int currentPiece = NULL;
-int nextPiece= NULL;
-int currentY= NULL;
-int currentX = NULL;
-int currentRotation = NULL;
+int currentPiece; // the current in play piece
+int nextPiece;
+int currentY;
+int currentX;
+int currentRotation;
 int board[BOARD_WIDTH][BOARD_HEIGHT];
 
 //Loads individual image as texture
@@ -318,10 +319,10 @@ SDL_Texture* loadTexture( std::string path );
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
 
-//The window renderer
+//The renderer
 SDL_Renderer* gRenderer = NULL;
 
-//Current displayed texture
+//The two textures
 SDL_Texture* blockTexture = NULL;
 SDL_Texture* borderTexture = NULL;
 
@@ -441,33 +442,31 @@ SDL_Texture* loadTexture( std::string path )
 
 	return newTexture;
 }
-//void creatNewPiece()
-//{
-//	int kind
-//	piece[kind][rotation][horizontal][vertical]
-//}
-
+//This method loops through to board array and draws the board itself
 void drawBoard()
 {
-	int count = 0;
 	for( int i = 0; i < BOARD_WIDTH; i++)
 	{
 		for( int j = 0; j < BOARD_HEIGHT; j++)
 		{
 			if( board[i][j] == POS_TAKEN )
 			{
-				SDL_Rect a = {i * 11 /*X*/, j * 11 /*Y*/, 10/*size*/,10/*size*/};
+				//create 2d rectangle to apply texture to, MAGNIFICATION defines the size of the rendered game
+				SDL_Rect a = {i * MAGNIFICATION /*X*/, j * MAGNIFICATION /*Y*/, 10/*size*/,10/*size*/};
+				//actually tell SDL to render the blockTexture to the screen
 				SDL_RenderCopy( gRenderer, blockTexture, NULL, &a );
 			}
-			else if( board[i][j] == BORDER )
+			else if( board[i][j] == BORDER ) //Same thing as above, expect loading borderTexture instead of the block texture
 			{
-				SDL_Rect a = {i * 11 /*X*/, j * 11 /*Y*/, 11/*size*/,11/*size*/};
+				//The size is 11 so there is no gap between the drawn border blocks
+				SDL_Rect a = {i * MAGNIFICATION /*X*/, j * MAGNIFICATION /*Y*/, 11/*size*/,11/*size*/};
 				SDL_RenderCopy( gRenderer, borderTexture, NULL, &a );
 			}
 		}
 	}
 }
 
+//This method creates a 2X2 array that is used to define where blocks are drawn
 void initBoard()
 {
     for (int x = 0; x < BOARD_WIDTH; x++)
@@ -507,7 +506,8 @@ bool collision()
     {
         for (int j = 0; j < PIECE_BLOCKS; j++)
         {   
-			if( pieces[currentPiece][currentRotation][i][j] == POS_TAKEN && ((board[i + currentX][j + currentY] == POS_TAKEN) || (board[i + currentX][j + currentY] == BORDER)) )
+			if( pieces[currentPiece][currentRotation][i][j] == POS_TAKEN && ((board[i + currentX][j + currentY] == POS_TAKEN)
+				|| (board[i + currentX][j + currentY] == BORDER)) )
 			{
 				return true;
 			}
@@ -524,12 +524,13 @@ void drawPiece()
         {   
 			if( pieces[currentPiece][currentRotation][i][j] == POS_TAKEN )
 			{
-				SDL_Rect a = {(i + currentX) * 11 /*X*/, (j + currentY) * 11 /*Y*/, 10/*size*/,10/*size*/};
+				SDL_Rect a = {(i + currentX) * MAGNIFICATION /*X*/, (j + currentY) * MAGNIFICATION /*Y*/, 10/*size*/,10/*size*/};
 				SDL_RenderCopy( gRenderer, blockTexture, NULL, &a );
 			}
         }
     }
 }
+//This method moves the piece and calls the collision method, if collision is true, revert the movement
 void movePiece()
 {
 	currentY++;
@@ -604,31 +605,18 @@ int main( int argc, char* args[] )
 						}
 					}
 				}
-
 				//Clear screen
 				SDL_RenderClear( gRenderer );
 				drawBoard();
 				drawPiece();
 				movePiece();
-				//collision();
-
 
 				//Update screen
 				SDL_RenderPresent( gRenderer );
 				SDL_Delay(50);
-
-				//when moving piece, set old piece spots to zero before drawing next piece
-
-				//MovePiece() Check if valid
-				//drawPiece()
-				//check for next move collision
-				//check for completed lines
-				//create new piece if needed
 			}
 		}
 	}
-
-	//Free resources and close SDL
 	close();
 
 	return 0;
